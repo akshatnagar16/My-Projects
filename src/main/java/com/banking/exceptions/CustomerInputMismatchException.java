f7740c7a6c13ae0b037ebd1e76d1ebd4434a0f0c
Akshat Nagar <62827202+akshatnagarxd@users.noreply.github.com>
2021-07-31 22:31:49 +0530

    Update CustomerInputMismatchException.java

--- src/main/java/com/banking/exceptions/CustomerInputMismatchException.java
+++ src/main/java/com/banking/exceptions/CustomerInputMismatchException.java
@@ -1,13 +1,12 @@
 package com.banking.exceptions;
-//package com.revature.iba.exceptions;
-//
-//import java.util.InputMismatchException;
-//
-//public class CustomerInputMismatchException extends InputMismatchException {
-//	public CustomerInputMismatchException() {
-//		// TODO Auto-generated constructor stub
-//	}
-//	public CustomerInputMismatchException(String message) {
-//		super(message);
-//	}
-//}
+
+import java.util.InputMismatchException;
+
+public class CustomerInputMismatchException extends InputMismatchException {
+	public CustomerInputMismatchException() {
+		// TODO Auto-generated constructor stub
+}
+	public CustomerInputMismatchException(String message) {
+		super(message);
+	}
+}
